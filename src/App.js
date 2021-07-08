b1b2fb6c8e9a90955827a1a1e56212d8eaa92fc2
Mauro Delmondes <derumondesu@gmail.com>
2021-07-08 14:04:11 -0400

    Creating & Styling Sidebar (incomplete)

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import { BrowserRouter as Router } from 'react-router-dom';
+import Sidebar from './components/Sidebar';
 
 function App() {
   return (
     <Router>
+      <Sidebar />
       <Navbar />
     </Router>
   );
-}
+};
 
 export default App;
---
+++ src/components/Sidebar/SidebarElements.js
@@ -0,0 +1,36 @@
+import styled from 'styled-components';
+import { FaTimes } from 'react-icons/fa';
+
+export const SidebarContainer = styled.aside`
+    position: fixed;
+    z-index: 999;
+    width: 100%;
+    height: 100%;
+    background: #0D0D0D;
+    display: grid;
+    align-items: center;
+    top: 0;
+    left: 0;
+    transition: 0.3s    ease-in-out;
+    // opacity: ${({ isOpen }) => ( isOpen ? '100%' : '0' )};
+    // top: ${({ isOpen }) => ( isOpen ? '0' : '-100%' )};
+    top: 0;
+`;
+
+export const CloseIcon = styled(FaTimes)`
+    color: #FFFFFF;
+`;
+
+export const Icon = styled.div`
+    position: absolute;
+    top: 1.2rem;
+    right: 1.5rem;
+    background: transparent;
+    font-size: 2rem;
+    cursor: pointer;
+    outline: none;
+`;
+
+export const SidebarWrapper 
+
+//59:06
